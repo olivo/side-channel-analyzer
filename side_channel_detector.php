a38eab901816e9413e864a2b2cccf47f39929353
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-02-05 23:50:08 -0600

    Added side channel analysis

--- side_channel_detector.php
+++ side_channel_detector.php
@@ -1,6 +1,6 @@
 <?php
 
-
+include_once(dirname(__FILE__) . '/dataflow_side_channel_analysis.php');
 include_once(dirname(__FILE__) . '/TaintPHP/PHP-Parser-master/lib/bootstrap.php');
 include_once(dirname(__FILE__) . '/TaintPHP/TaintAnalysis/taint_analysis.php');
 include_once(dirname(__FILE__) . '/TaintPHP/CFG/CFG.php');
@@ -10,6 +10,11 @@ $filename = $argv[1];
 // Obtain the CFGs of the main function, auxiliary functions and function signatures.
 $file_cfgs = CFG::construct_file_cfgs($filename);
 
-$tainted_maps = taint_analysis($file_cfgs[0],$file_cfgs[1],$file_cfgs[2]);
+print "==== STARTING TAINT ANALYSIS ====\n";
+$tainted_maps = taint_analysis($file_cfgs[0], $file_cfgs[1], $file_cfgs[2]);
+
+print "==== STARTING SIDE-CHANNEL DETECTION ====\n";
+
+dataflow_side_channel_detection($file_cfgs[0], $file_cfgs[1], $file_cfgs[2]);
 
 ?>
