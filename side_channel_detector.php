1a7a886544c25cd4475b7bd6c6b42f4c27754756
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-02-26 22:22:15 -0600

    Added directory iterator to construct CFG over all files in a project

--- side_channel_detector.php
+++ side_channel_detector.php
@@ -5,18 +5,29 @@ include_once(dirname(__FILE__) . '/TaintPHP/PHP-Parser-master/lib/bootstrap.php'
 include_once(dirname(__FILE__) . '/TaintPHP/TaintAnalysis/taint_analysis.php');
 include_once(dirname(__FILE__) . '/TaintPHP/CFG/CFG.php');
 
-$filename = $argv[1];
+$projectPath = $argv[1];
 
-print "==== STARTING ANALYSIS ON FILE: " . $filename . "\n";
+// Iterating over all php files in a project path.
+$Directory = new RecursiveDirectoryIterator($projectPath);
+$Iterator = new RecursiveIteratorIterator($Directory);
+$Regex = new RegexIterator($Iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
+$Regex->rewind();
+
+while($Regex->valid()) {
+        // Regex iterator contains an array of a single element for each file.
+        $fileName = $Regex->current()[0];
+	print "==== STARTING ANALYSIS ON FILE: " . $fileName . "\n";
 	
-// Obtain the CFGs of the main function, auxiliary functions and function signatures.
-$file_cfgs = CFG::construct_file_cfgs($filename);
+	// Obtain the CFGs of the main function, auxiliary functions and function signatures.
+	$file_cfgs = CFG::construct_file_cfgs($fileName);
 
-print "==== STARTING TAINT ANALYSIS ====\n";
-$file_tainted_maps = taint_analysis($file_cfgs[0], $file_cfgs[1], $file_cfgs[2]);
+	//print "==== STARTING TAINT ANALYSIS ====\n";
+	//$file_tainted_maps = taint_analysis($file_cfgs[0], $file_cfgs[1], $file_cfgs[2]);
 
-print "==== STARTING SIDE-CHANNEL DETECTION ====\n";
+	//print "==== STARTING SIDE-CHANNEL DETECTION ====\n";
 
-dataflow_side_channel_detection($file_cfgs[0], $file_cfgs[1], $file_cfgs[2], $file_tainted_maps);
+	//dataflow_side_channel_detection($file_cfgs[0], $file_cfgs[1], $file_cfgs[2], $file_tainted_maps);
+	$Regex->next();
+}
 
 ?>
